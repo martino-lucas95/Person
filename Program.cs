cf0d7582df208defe0419e4813f841e6fdb23cc3
fmachadopiriz <fmachadopiriz@outlook.com>
2018-08-15 08:12:13 -0300

    Commit inicial

--- Program.cs
+++ Program.cs
@@ -22,10 +22,10 @@ namespace PosterObjectsClassesMessages
     {
         static void Main(string[] args)
         {
-            Person juan = new Person("Juan Pérez", "1.234.567-8");
-            Person maria = new Person("María García", "8.765.432-1");
-            juan.IntroduceYourself();
-            maria.IntroduceYourself();
+            Person john = new Person("John Doe", "1.234.567-8");
+            Person jane = new Person("Jane Doe", "8.765.432-1");
+            john.IntroduceYourself();
+            jane.IntroduceYourself();
         }
     }
 }
